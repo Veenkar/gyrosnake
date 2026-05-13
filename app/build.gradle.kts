d1f559b05ef9ade820295c2d00f5493fca05d2cb
Veenkar <veenkar@gmail.com>
2026-05-13 22:51:24 +0200

    bump version to 4(1.3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.gyrosnake"
         minSdk = 31          // Android 12
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     buildTypes {
